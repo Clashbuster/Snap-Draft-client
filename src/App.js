201ebc867a19f24c54f99187d4afed3f1db0d81c
github username <github email address>
2020-01-28 10:44:17 -0800

    first frontend commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import React from 'react.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let example = ''
+
+class App extends React.Component {
+
+signup (e){
+  e.preventDefault()
+  fetch('http://localhost:3000/api/v1/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      user: {
+        username: 'guy215',
+        password: 'hi'
+      }
+    })
+  })
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+    })
+}
+
+login(e){
+  e.preventDefault()
+  fetch('http://localhost:3000/api/v1/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+      Accept: 'application/json'
+  },
+  body: JSON.stringify({
+    user: {
+      username: 'guy215',
+      password: 'hi'
+    }
+  })
+})
+  .then(r => r.json())
+    .then(console.log)
+}
+
+
+
+render (){
+    return (
+      <div className="App">
+        <button onClick={e => this.signup(e)}>make new user with salt</button>
+        <button onClick={e => this.login(e)}>login with jwt</button>
+      </div>
+    );
+  }
 }
 
 export default App;
